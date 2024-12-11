adfd45128527133017c583f791985f247212f67d
Khanh Dan Nguyen Nhat <dan.nhatkhanh@gmail.com>
2024-12-11 23:24:12 +0700

    feat: add tabs with sidebar

--- components/Home/index.tsx
+++ components/Home/index.tsx
@@ -1,5 +1,115 @@
-import React from 'react';
+'use client';
+import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react';
+import {
+  faCartShopping,
+  faHome,
+  faWallet,
+  faTableCellsLarge,
+  faShop,
+  faRobot,
+  faTools,
+  faDice,
+  faUsers,
+  faThumbsUp,
+  faSackDollar,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
 
 export function HomeComponent() {
-  return <div>test</div>;
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const currentTab = searchParams.get('tab');
+  const [selectedIndex, setSelectedIndex] = useState(Number(currentTab) ?? 0);
+  const getTabClasses = (selected: boolean) =>
+    `${
+      selected
+        ? 'bg-white/10 text-tapps-blue focus:outline-none border-r-tapps-blue'
+        : 'bg-transparent text-white hover:bg-gray-200 border-transparent focus:outline-none'
+    } px-4 py-2 text-base border-r-2`;
+
+  const onRenderTabs = () => {
+    const tabList = [
+      {
+        name: 'Home',
+        icon: <FontAwesomeIcon icon={faHome} />,
+      },
+      {
+        name: 'Most Popular',
+        icon: <FontAwesomeIcon icon={faUsers} />,
+      },
+      {
+        name: 'Wallets',
+        icon: <FontAwesomeIcon icon={faWallet} />,
+      },
+      {
+        name: 'Exchanges',
+        icon: <FontAwesomeIcon icon={faTableCellsLarge} />,
+      },
+      {
+        name: 'Marketpaces',
+        icon: <FontAwesomeIcon icon={faShop} />,
+      },
+      {
+        name: 'Game',
+        icon: <FontAwesomeIcon icon={faRobot} />,
+      },
+      {
+        name: 'Staking',
+        icon: <FontAwesomeIcon icon={faSackDollar} />,
+      },
+      {
+        name: 'Social',
+        icon: <FontAwesomeIcon icon={faThumbsUp} />,
+      },
+      {
+        name: 'Gambling',
+        icon: <FontAwesomeIcon icon={faDice} />,
+      },
+      {
+        name: 'Tools',
+        icon: <FontAwesomeIcon icon={faTools} />,
+      },
+      {
+        name: 'Shopping',
+        icon: <FontAwesomeIcon icon={faCartShopping} />,
+      },
+    ];
+
+    return tabList.map((tab) => (
+      <Tab
+        key={tab.name}
+        className={({ selected }) =>
+          `${getTabClasses(selected)} flex items-center gap-5 py-3 hover:bg-white/10`
+        }
+      >
+        {tab.icon}
+        <p>{tab.name}</p>
+      </Tab>
+    ));
+  };
+  return (
+    <div>
+      <TabGroup
+        selectedIndex={selectedIndex}
+        onChange={(index) => {
+          setSelectedIndex(index);
+          router.push(`?tab=${index}`);
+        }}
+        className="flex min-h-[calc(100dvh-60.5px)] w-full"
+      >
+        <TabList className="flex min-w-[15%] flex-col bg-tapps-light-black">
+          {onRenderTabs()}
+        </TabList>
+        <TabPanels>
+          <TabPanel>Content 1</TabPanel>
+          <TabPanel>Content 2</TabPanel>
+          <TabPanel>Content 3</TabPanel>
+          <TabPanel>Content 4</TabPanel>
+          <TabPanel>Content 5</TabPanel>
+        </TabPanels>
+      </TabGroup>
+    </div>
+  );
 }
