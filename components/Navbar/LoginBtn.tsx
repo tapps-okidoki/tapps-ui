c273160d330510c7ac574064db8a268a684d8e25
Khanh Dan Nguyen Nhat <dan.nhatkhanh@gmail.com>
2024-12-29 11:46:46 +0700

    test get user data

--- components/Navbar/LoginBtn.tsx
+++ components/Navbar/LoginBtn.tsx
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export function LoginBtn() {
+  const [tgAuthResult, setTgAuthResult] = useState<string | null>(null);
+  console.log('tgAuthResult: ', tgAuthResult);
+  const a = decodeURIComponent(tgAuthResult ?? '');
+  console.log('a: ', a);
+
+  useEffect(() => {
+    // Extract the tgAuthResult from the URL hash
+    const urlHash = window.location.hash;
+    console.log('urlHash: ', urlHash);
+    const tgAuthResultParam = new URLSearchParams(urlHash.replace('#', '')).get(
+      'tgAuthResult',
+    );
+
+    if (tgAuthResultParam) {
+      setTgAuthResult(tgAuthResultParam);
+    }
+  }, []);
+
   const handleLogin = () => {
     const botId = '7696468458'; // Replace with your bot's ID
-    const origin = encodeURIComponent(`https://tapps-okidoki.vercel.app/`);
-    const returnTo = encodeURIComponent(
-      `https://tapps-okidoki.vercel.app/login-success`,
-    );
+    const origin = encodeURIComponent('https://tapps-okidoki.vercel.app/'); // Your origin
+    const returnTo = encodeURIComponent('https://tapps-okidoki.vercel.app/'); // Redirect URL after login
     const telegramUrl = `https://oauth.telegram.org/auth?bot_id=${botId}&origin=${origin}&request_access=write&return_to=${returnTo}`;
-
-    window.open(telegramUrl, '_blank');
+    window.location.href = telegramUrl;
+    // window.open(telegramUrl); // Open Telegram login in a new tab
   };
+
   return (
     <button className="py-2" onClick={handleLogin}>
-      Log in
+      Log in with Telegram
     </button>
   );
 }
